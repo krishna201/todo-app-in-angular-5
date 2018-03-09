f12636677c11c508ce2beab77a6b06f833cff97e
krishna201 <krishnahare201@gmail.com>
2018-03-09 20:12:33 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { element } from 'protractor';
+import { HttpClient } from '@angular/common/http';
+import { userInfo } from 'os';
+import jsondata from './data';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,113 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  edited = false;
+  added = true;
+  arraydata: Array<any> = [];
+  userid: number;
+
+  name: any;
+  email: any;
+
+  quoteCollection2: {
+    id: number;
+    name: string;
+    email: string;
+  }[];
+  constructor(private httpClient: HttpClient) {
+    // for (let i = 0; i < jsondata.length; i++) {
+    //   console.log(jsondata[i]);
+    //   this.quoteCollection2 = jsondata[i];
+    //   this.arraydata.push(jsondata[i]);
+    // }
+    // console.log(this.arraydata);
+    // this.quoteCollection2 = this.arraydata;
+
+    this.get_all_data();
+
+  }
+
+  get_all_data() {
+
+     this.httpClient
+      .get('http://localhost:8088/api2/get_all_data')
+      .subscribe(data => {
+        console.log( data );
+        this.quoteCollection2 = data[0];
+      } );
+
+  }
+
+  updatedata( quoteGroup ) {
+    console.log( quoteGroup );
+    this.edited = true;
+    this.added = false;
+    this.name = quoteGroup.name;
+    this.email = quoteGroup.email;
+    this.userid = quoteGroup.userid;
+  }
+
+
+  adduserdata() {
+     const userdetail = {
+      name: this.name,
+      email: this.email
+     };
+    console.log( userdetail );
+     this.httpClient
+      .get('http://localhost:8088/api2/add_data?name='  + userdetail.name + '&email=' + userdetail.email)
+      .subscribe(data => {
+        console.log( data );
+        this.name = '';
+        this.email = '';
+        this.get_all_data();
+      } );
+  }
+
+
+
+  deletedata( quoteGroup ) {
+      this.httpClient
+      .get('http://localhost:8088/api2/delete_data?userid='  + quoteGroup.userid)
+      .subscribe(data => {
+        console.log( data );
+         this.get_all_data();
+      } );
+  }
+
+  updateuserdata() {
+    const userdetail = { name: this.name, email: this.email };
+    console.log( this.userid );
+    console.log( userdetail );
+
+     this.httpClient
+      .get('http://localhost:8088/api2/update_data?userid='  + this.userid + '&name=' + userdetail.name + '&email=' + userdetail.email)
+      .subscribe(data => {
+        console.log( data );
+        this.name = '';
+        this.email = '';
+        this.added = true;
+        this.edited = false;
+         this.get_all_data();
+      } );
+}
+
+uploadFile(event){
+  const elem = event.target;
+  // console.log(elem)
+  if ( elem.files.length > 0 ) {
+    console.log( elem.files[0] );
+    const formData = new FormData();
+    formData.append( 'file', elem.files[0] );
+    this.httpClient
+      .post( 'http://localhost:8088/api2/uploadimage', formData )
+      .subscribe( ( data ) => {
+        console.log( data );
+      }, ( error ) => {
+        console.log( error );
+      } );
+
+  }
+}
+
 }
---
+++ src/app/data.ts
@@ -0,0 +1,11 @@
+export default [{
+    'id': 1,
+    'name': 'krishna',
+    'email': 'krishna@gmail.com'
+},
+  {
+    'id': 2,
+    'name': 'ram',
+    'email': 'ram@gmail.com'
+}
+];
